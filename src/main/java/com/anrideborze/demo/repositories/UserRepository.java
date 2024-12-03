560eb9c180c9e3209eef91ac17fcfab84686b2cb
anrideborze <anrideborze@gmail.com>
2024-12-03 18:39:15 +0200

    added default code to UserRepository

--- src/main/java/com/anrideborze/demo/repositories/UserRepository.java
+++ src/main/java/com/anrideborze/demo/repositories/UserRepository.java
@@ -1,2 +1,10 @@
-package com.anrideborze.demo.repositories;public class UserRepository {
+package com.anrideborze.demo.repositories;
+
+import com.anrideborze.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+        import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
