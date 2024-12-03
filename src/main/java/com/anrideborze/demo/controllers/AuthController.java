a796aa4988b332871fee2de3dc749ece57213624
anrideborze <anrideborze@gmail.com>
2024-12-03 18:40:17 +0200

    added default code to AuthController

--- src/main/java/com/anrideborze/demo/controllers/AuthController.java
+++ src/main/java/com/anrideborze/demo/controllers/AuthController.java
@@ -1,2 +1,29 @@
-package com.anrideborze.demo.controllers;public class AuthController {
+package com.anrideborze.demo.controllers;
+
+import com.anrideborze.demo.entities.User;
+import com.anrideborze.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username is already taken.");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully!");
+    }
 }
