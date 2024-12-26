80dc56e7dcddfc6de385789b11452e4d9bef4452
anrideborze <anrideborze@gmail.com>
2024-12-26 20:37:57 +0200

    adding registration to AuthController

--- src/main/java/com/anrideborze/demo/controllers/AuthController.java
+++ src/main/java/com/anrideborze/demo/controllers/AuthController.java
@@ -1,27 +1,34 @@
 package com.anrideborze.demo.controllers;
 
 import com.anrideborze.demo.dto.RegisterRequest;
+import com.anrideborze.demo.entities.User;
+import com.anrideborze.demo.enums.Role;
+import com.anrideborze.demo.repositories.UserRepository;
 import com.anrideborze.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest request) {
-        try {
-            userService.registerUser(request.getUsername(), request.getPassword(), request.getEmail(), request.getRole());
-            return ResponseEntity.ok("User registered successfully!");
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username is already taken.");
         }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.addRole(Role.USER); // Додаємо роль USER за замовчуванням
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully!");
     }
+
 }
