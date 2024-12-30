6c091fc4148e8dc7ed397b367738960af71a6797
anrideborze <anrideborze@gmail.com>
2024-12-30 17:01:10 +0200

    refactoring AuthController class, loginUser method with using login method in AuthService

--- src/main/java/com/anrideborze/demo/controllers/AuthController.java
+++ src/main/java/com/anrideborze/demo/controllers/AuthController.java
@@ -1,24 +1,14 @@
 package com.anrideborze.demo.controllers;
 
 import com.anrideborze.demo.dto.LoginRequest;
-import com.anrideborze.demo.dto.RegisterRequest;
 import com.anrideborze.demo.entities.User;
-import com.anrideborze.demo.enums.Role;
 import com.anrideborze.demo.repositories.UserRepository;
 import com.anrideborze.demo.services.AuthService;
-import com.anrideborze.demo.services.UserService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -44,29 +34,11 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
-        Optional<User> userOpt = userRepository.findByUsername(loginRequest.getUsername());
-        if (userOpt.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password.");
+        try {
+            return ResponseEntity.ok(authService.login(loginRequest));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(401).body(e.getMessage());
         }
-
-        User user = userOpt.get();
-        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password.");
-        }
-
-        // Створення JWT токену
-        String token = Jwts.builder()
-                .setSubject(user.getUsername())
-                .claim("roles", user.getRoles()) // Додаємо ролі
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 день
-                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS512)) // Генерація підпису
-                .compact();
-
-        return ResponseEntity.ok(new HashMap<>() {{
-            put("token", token);
-            put("message", "Login successful.");
-        }});
     }
 
 
